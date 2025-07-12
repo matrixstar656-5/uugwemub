308e2987bd8cc6916aa46cc3f7783a42fcd2ec4b
matrixstar656-5 <matrixstar656@gmail.com>
2025-07-12 03:27:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ from flask_socketio import SocketIO, send
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+chat_history = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('connect')
+def handle_connect():
+    for msg in chat_history:
+        send(msg)
+        
 @socketio.on('message')
 def handle_message(msg):
     print(f'Message: {msg}')
+    chat_history.append(msg)
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
