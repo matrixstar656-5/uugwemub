947932a01d0be689f87cbc2f7f26ca8360fe5cf8
matrixstar656-5 <matrixstar656@gmail.com>
2026-04-05 16:56:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 chat_history = []
-users = {}
 
 host_sid = None
 pending_users = {}   # sid -> username
 approved_users = {}  # sid -> username
+
 
 @app.route('/')
 def index():
@@ -30,21 +30,32 @@ def handle_connect():
 @socketio.on('join')
 def handle_join(username):
     global host_sid
+
     sid = request.sid
+
+    # First user becomes host
     if host_sid is None:
         host_sid = sid
         approved_users[sid] = username
+
+        # 🔥 IMPORTANT CHANGE (host event)
+        emit('you_are_host', room=sid)
+
         emit('message', f'--- {username} (host) joined ---', broadcast=True)
         emit('user_list', list(approved_users.values()), broadcast=True)
         return
 
+    # Others go to pending
     pending_users[sid] = username
 
-    emit('waiting')
+    emit('waiting', room=sid)
+
+    # Notify host
     emit('approval_request', {
         'sid': sid,
         'username': username
     }, room=host_sid)
+
 
 @socketio.on('approve_user')
 def approve_user(sid):
@@ -68,14 +79,23 @@ def reject_user(sid):
 @socketio.on('disconnect')
 def handle_disconnect():
     global host_sid
+
     sid = request.sid
+
     username = approved_users.pop(sid, None) or pending_users.pop(sid, None)
+
+    # If host leaves → assign new host
     if sid == host_sid:
         if approved_users:
             host_sid = next(iter(approved_users))
+
+            # 🔥 ALSO IMPORTANT: promote new host
+            emit('you_are_host', room=host_sid)
+
             emit('message', f'--- New host assigned ---', room=host_sid)
         else:
             host_sid = None
+
     if username:
         emit('message', f'--- {username} left ---', broadcast=True)
         emit('user_list', list(approved_users.values()), broadcast=True)
@@ -84,10 +104,10 @@ def handle_disconnect():
 @socketio.on('message')
 def handle_message(msg):
     if request.sid not in approved_users:
-        return
+        return  # block unapproved users
+
     chat_history.append(msg)
     send(msg, broadcast=True)
-
 
 
 if __name__ == '__main__':
